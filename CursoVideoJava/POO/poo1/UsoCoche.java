83482f58830d9c8d6aa00744e7d7b7518874cb7e
iremjm <iremti2@gmail.com>
2016-11-30 20:00:50 +0100

    Programa coches

--- CursoVideoJava/POO/poo1/UsoCoche.java
+++ CursoVideoJava/POO/poo1/UsoCoche.java
@@ -3,10 +3,20 @@ package poo1;
 public class UsoCoche {
 
 	public static void main(String[] args) {
+		//Instanciar  clases, ejemplar de clases Coches
+		Coche c1=new Coche();
+		Coche c2=new Coche();
 		
-		Coche c1=new Coche();//Instanciar una clase,ejemplar de clase Coche
-		System.out.println("este coche tiene "+ c1.ruedas+ " ruedas");
-
+		
+		
+		c2.estableceColor("amarillo");
+		
+		System.out.println(c2.dimeDatosGenerales());
+		System.out.println(c2.dimeColor());
+		
+		c1.configuraAsientos("no");
+		System.out.println(c1.dimeAsientos());
+		
 	}
 
 }
