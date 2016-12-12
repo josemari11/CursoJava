0d660a29c7794d07fbbbb00ee0d2c2fc0f60686c
iremjm <iremti2@gmail.com>
2016-12-12 20:25:05 +0100

    Clase con un único fichero

---
+++ CursoVideoJava/POO/poo1/UsoEmpleado.java
@@ -0,0 +1,22 @@
+package poo1;
+
+import java.util.Date;
+
+public class UsoEmpleado {
+
+	public static void main(String[] args) {
+		
+
+	}//class UsoEmpleado
+
+}//void
+//Metodo constructor
+class Empleado{
+	public Empleado(String nom, double sueldo, int anno, int mes, int dia){
+		
+	}
+	
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+}//class Empleado
