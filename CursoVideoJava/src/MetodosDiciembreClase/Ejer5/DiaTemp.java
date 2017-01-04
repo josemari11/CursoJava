1de0e1867fc06601d4334d4762e8b46cc9a00732
House <House>
2017-01-04 09:19:27 +0100

    Añadidos ejercicios sin terminar

---
+++ CursoVideoJava/src/MetodosDiciembreClase/Ejer5/DiaTemp.java
@@ -0,0 +1,12 @@
+/*5)Refactoriza el ejercicio 1 para utilizar una programación orientada objetos. Por ejemplo:
+	-define la clase DiaTemp, con los atributos:dia, minT y maxT y los métodos:pideTemps()->pide
+	por consola la temperatura máxima y mínima de ese día y las guarda en sus atributos privados;
+	double getTempMedia(); String toString()->imprime por consola el día la tempMax y min.
+	Define la clase DiasTemp, con al menos el atributo DiaTemp[] dias;
+	en la constructora se le pasarán en número de días máximo que queremos guardar…
+	*/
+package MetodosDiciembreClase.Ejer5;
+
+public class DiaTemp {
+
+}
---
+++ CursoVideoJava/src/MetodosDiciembreClase/Ejer5/Programa.java
@@ -0,0 +1,17 @@
+/*5)Refactoriza el ejercicio 1 para utilizar una programación orientada objetos. Por ejemplo:
+-define la clase DiaTemp, con los atributos:dia, minT y maxT y los métodos:pideTemps()->pide
+por consola la temperatura máxima y mínima de ese día y las guarda en sus atributos privados;
+double getTempMedia(); String toString()->imprime por consola el día la tempMax y min.
+Define la clase DiasTemp, con al menos el atributo DiaTemp[] dias;
+en la constructora se le pasarán en número de días máximo que queremos guardar…
+*/
+package MetodosDiciembreClase.Ejer5;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
